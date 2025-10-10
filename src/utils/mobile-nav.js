57ad9fda9bfd063a69e5a3a6b782188e87072192
Gyon Shayel Joshua <gyon.perera2002@gmail.com>
2025-10-10 17:28:21 +0400

    Enhancements for mobile-nav

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -10,36 +10,41 @@ const mobileNav = () => {
 
   headerBtn.addEventListener("click", () => {
     headerBtn.classList.toggle("active");
+    headerLogoLink.classList.toggle("hidden");
 
     isMobileNavOpen = !isMobileNavOpen;
 
     if (isMobileNavOpen) {
-      headerLogoLink.hidden = true;
+      // headerLogoLink.hidden = true;
       mobileNav.classList.add("active");
       mobileNav.classList.remove("not-active");
       // mobileNav.style.display = "flex";
       document.body.style.overflowY = "hidden";
+      document.documentElement.style.overflow = "hidden"; // For iOS
     } else {
-      headerLogoLink.hidden = false;
+      // headerLogoLink.hidden = false;
       mobileNav.classList.add("not-active");
       mobileNav.classList.remove("active");
       // mobileNav.style.display = "none";
       document.body.style.overflowY = "auto";
+      document.documentElement.style.overflow = "auto";
     }
   });
 
   window.addEventListener("resize", () => {
     if (window.innerWidth >= 768 && isMobileNavOpen) {
       isMobileNavOpen = false;
-      headerLogoLink.hidden = false;
+      // headerLogoLink.hidden = false;
       headerBtn.classList.remove("active");
+      headerLogoLink.classList.remove("hidden");
       // mobileNav.style.display = "none";
       mobileNav.classList.add("not-active");
       mobileNav.classList.remove("active");
       document.body.style.overflowY = "auto";
+      document.documentElement.style.overflow = "auto";
     }
 
-    // Expanding mobile header
+    // Expanding mobile header to match mobile-nav__link padding
     window.innerWidth < 768
       ? header.classList.remove("container")
       : header.classList.add("container");
@@ -48,12 +53,14 @@ const mobileNav = () => {
   mobileLinks.forEach((link) => {
     link.addEventListener("click", () => {
       isMobileNavOpen = false;
-      headerLogoLink.hidden = false;
+      // headerLogoLink.hidden = false;
       headerBtn.classList.remove("active");
+      headerLogoLink.classList.remove("hidden");
       // mobileNav.style.display = "none";
       mobileNav.classList.add("not-active");
       mobileNav.classList.remove("active");
       document.body.style.overflowY = "auto";
+      document.documentElement.style.overflow = "auto";
     });
   });
 };
